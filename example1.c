8219f087897a33ad6440ab3c13842a43e2c2a719
ahorie <akihiro.horie@avx.com>
2018-12-25 16:12:52 -0800

    Made edits to example1.c, made executable

--- example1.c
+++ example1.c
@@ -1,12 +1,16 @@
-char c;
-unsigned char uc;
-short s;
-unsigned short us;
-int i;
-unsigned u;
-long l;
-unsigned long ul;
-float f;
-double d;
-long double ld;
-const int ci;
+#include <stdio.h>
+#include <float.h>
+#include <limits.h>
+
+int main()
+{
+	unsigned long long ullmax = ULLONG_MAX;
+	long lmax = LONG_MAX;
+	long double ldmax = LDBL_MAX;
+
+	printf("The max value of an unsigned long long is %Lu.\n", ullmax);
+	printf("The max value of a long is %ld.\n", lmax);
+	printf("The max value of a long double is %Lf.\n", ldmax);
+
+	return 0;
+}
