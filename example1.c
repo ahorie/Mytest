be43d29782d3fae87309d729038c8d61608f5756
ahorie <akihiro.horie@avx.com>
2019-01-09 10:07:04 -0800

    Learning c

--- example1.c
+++ example1.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int main()
-{	
-	char February;
-	printf("If this year is leap year, enter 1.  "
-			"If not, enter any integer: ");
-	scanf("%c", &February);
+{
+	char c;
+	printf("Enter an alphabet: ");
+	scanf("%c", &c);
 
-	int days = (February == '1') ? 29 : 28;
-	printf("Number of days in February = %d\n", days);
+	int isLowerCaseVowel, isUpperCaseVowel;
+
+	isLowerCaseVowel = (c == 'a' | c = 'e' | c = 'i' | c = 'o' | c = 'u') ? 1 : 0;
+	isUpperCaseVowel = (c == 'A' | c = 'E' | c = 'I' | c = 'O' | c = 'U') ? 1 : 0;
+
+	if (isLowerCaseVowel + isUpperCaseVowel == 1)
+		printf("The character is a vowel");
+	else
+		printf("The character is a consonant");
 
 	return 0;
 }
+
